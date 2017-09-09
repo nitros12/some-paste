3114fc6086517ffc90f07a9062448dddac691e56
ben simms <ben@bensimms.moe>
2017-09-09 03:36:08 +0100

    add highlighter helper function

---
+++ src/Highlight.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Highlight ( highlightPaste
+                 ) where
+
+import           Data.Text       (Text)
+import Data.Text as T
+
+import           Skylighting
+
+import           Text.Blaze.Html
+
+-- |A helper function to highlight a paste
+highlightPaste :: Text -> Text -> IO Html
+highlightPaste code syntax = do
+  syntax' <- case lookupSyntax syntax defaultSyntaxMap of
+    Just s -> return s
+    Nothing -> fail . T.unpack $ T.append "no available syntax for: " syntax
+    -- TODO: if this fails, instead just format into lines
+  let opts = defaultFormatOpts { numberLines = True
+                               , lineAnchors = True
+                               }
+
+  let config = TokenizerConfig { syntaxMap = defaultSyntaxMap
+                               , traceOutput = False
+                               }
+  sourceLines <- case tokenize config syntax' code of
+    Left e  -> fail e
+    Right v -> return v
+  return $ formatHtmlInline opts sourceLines
