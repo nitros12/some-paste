086c602fd189f027c48ed7bd2e12ac045df17a94
ben simms <ben@bensimms.moe>
2017-09-14 00:27:42 +0100

    fixup

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE Strict            #-}
+{-# LANGUAGE OverloadedStrings   #-}
 
 module Main where
 
-import           Data.Text                            (Text)
-import qualified Data.Text                            as T
+import qualified Data.Text                            as ST
 import           Data.Text.Format
-import           Data.Text.Lazy                       (toStrict)
+import           Data.Text.Lazy                       (Text, toStrict)
+import           Data.Text.Lazy                       as T
 
 import           Text.Blaze.Html.Renderer.Text
 
@@ -19,9 +18,9 @@ import           Network.HTTP.Types.Status
 
 import           Database.PostgreSQL.Simple
 
+import           Data.Char
 import           Data.Pool
 import           System.Random
-import Data.Char
 
 import           Web.Scotty
 import           Web.Scotty.Internal.Types
@@ -51,32 +50,40 @@ randomHash = do
   rands <- replicateM 40 . onlyAlpha $ getStdRandom (randomR ('0', 'Z'))
   return $ T.pack rands
 
---maybeParam :: Text -> ActionM (Maybe Text)
-maybeParam params key = do -- TODO: make this be in the scotty monad
-  --paramList <- params
-  toStrict <$> lookup key params
+maybeParam :: Text -> ActionT Text IO (Maybe Text)
+maybeParam key = do
+  paramList <- params
+  return $ lookup key paramList
 
---retrievePaste :: ScottyT Text IO ()
+retrievePaste :: Connection -> ActionM ()
 retrievePaste conn = do
   key <- param "key"
-  prms <- params
-  let theme = maybeParam prms "theme"
+  theme <- maybeParam "theme"
   paste <- liftAndCatchIO $ getPaste conn key
   case paste of
     Just p -> html . renderHtml $ viewPaste p theme
     Nothing -> do
       status status404
-      html (format "Paste {} not found" [key])
+      html $ format "Paste {} not found" [key]
 
---savePaste :: ScottyT Text IO ()
+savePaste :: Connection -> ActionM ()
 savePaste conn = do
-  prms <- params
-  let lang = fromMaybe "plain" $ maybeParam prms "lang"
+  lang <- fromMaybe "plain" <$> maybeParam "lang"
   text <- param "text"
   key <- liftAndCatchIO randomHash
   liftAndCatchIO $ insertPaste conn text key lang
   status created201
   redirect (format "/paste/{}" [key])
+
+retrievePasteRaw :: Connection -> ActionM ()
+retrievePasteRaw conn = do
+  key <- param "key"
+  paste <- liftAndCatchIO $ getPaste conn key
+  case paste of
+    Just p -> text . plainPaste $ p
+    Nothing -> do
+      status status404
+      html $ format "Paste {} not found" [key]
 
 main = do
   pool <- createPool spawnConn close 2 10 5
@@ -85,7 +92,9 @@ main = do
       scottyOpts opts $ do
 
       middleware . gzip $ def { gzipFiles = GzipCompress }
-      middleware logStdoutDev
+      --middleware logStdoutDev
 
       get "/paste/:key" $ retrievePaste conn
+      get "/paste/raw/:key" $ retrievePasteRaw conn
       post "/paste" $ savePaste conn
+
--- src/Templates.hs
+++ src/Templates.hs
@@ -5,14 +5,16 @@ module Templates where
 import           Text.Blaze.Html5            as H
 import qualified Text.Blaze.Html5.Attributes as A
 
-import           Data.Text                   (Text)
-import qualified Data.Text                   as T
+import qualified Data.Text                   as ST
 import           Data.Text.Format
+import           Data.Text.Lazy              (toStrict, fromStrict, Text)
+import qualified Data.Text.Lazy              as T
 
 import           Data.Int                    (Int64)
 import           Data.Time.Clock
 
-import           Clay                        (height, margin, nil, pct, (?), (#))
+import           Clay                        (height, margin, nil, pct, ( # ),
+                                              (?))
 import qualified Clay                        as C
 
 import           Db                          (Paste)
@@ -21,7 +23,7 @@ import           Highlight
 viewPaste :: Paste -> Maybe Text -> Html
 viewPaste (_, time, paste, key, lang) theme = html $ do
   H.head . H.title . lazyText $ format "Paste: {}: {}" [key, lang]
-  body . pageBody $ highlightPaste paste lang theme
+  body . pageBody $ highlightPaste (toStrict paste) (toStrict lang) $ fmap toStrict theme
 
 
 pageBody :: Html -> Html
@@ -30,3 +32,6 @@ pageBody content = do
   style H.! A.type_ "text/css" $ toHtml . C.render $ do
     C.body ? margin nil nil nil nil
     C.table # ".sourceCode" ? height (pct 100)
+
+plainPaste :: Paste -> Text
+plainPaste (_, _, t, _, _) = t
